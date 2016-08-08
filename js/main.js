f958a1758a67f3712f590fa3cb9e9a69609a387e
Dana Ng <ngdana1@gmail.com>
2016-08-08 12:33:56 -0700

    added animation and removed text from works.html

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
  $(document).ready(function(){
     
 
-    // make the scrolling smoothier using jQuery scrollTop()
-    var $root = $('html, body');
-   $('a').click(function(){
-        $root.animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 800);
-    return false;
-});
+//     // make the scrolling smoothier using jQuery scrollTop()
+//     var $root = $('html, body');
+//    $('a').click(function(){
+//         $root.animate({
+//         scrollTop: $( $.attr(this, 'href') ).offset().top
+//     }, 800);
+//     return false;
+// });
+
+  $('.box').click(function(){
+    $(this).animate({
+        left: '250px',
+        height: '+=20px',
+        width: '+=20px'
+      });
+  });
+
+  $('.reset').on('click', function(){
+    $('.box').css('height', '100px');
+    $('.box').css('width', '100px');
+  });
+
  });
