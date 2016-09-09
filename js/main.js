34a80173b376bfc2bc72beb6373ad10e75b53f87
Dana Ng <ngdana1@gmail.com>
2016-09-09 10:43:28 -0700

    prettified files

--- js/main.js
+++ js/main.js
@@ -1,22 +1,19 @@
-$(function(){
-    
+$(function() {
+
     // jumbotronFn();
 
-     // jQuery animation for works.html page
+    // jQuery animation for works.html page
     $('.flexbox-container > div').hover(function() {
-      $(this).css('background-color', 'rgb(243, 195, 61)');
-      $(this).css('font-weight', 'bold');
-    },
-    function() {
-       $(this).css('background-color', '');
-      $(this).css('font-weight', '');
-    });
+            $(this).css('background-color', 'rgb(243, 195, 61)');
+            $(this).css('font-weight', 'bold');
+        },
+        function() {
+            $(this).css('background-color', '');
+            $(this).css('font-weight', '');
+        });
 
- });
+});
 
- // function jumbotronFn() {
- //    	$('.jumbotron').slideUp(7000);
- //   }
-    
-   
-
+// function jumbotronFn() {
+//      $('.jumbotron').slideUp(7000);
+//   }
