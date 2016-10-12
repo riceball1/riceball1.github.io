2d25b7451b145f0026c681e5e93a0a5c81735698
Dana Ng <ngdana1@gmail.com>
2016-10-12 14:50:24 -0700

    updated hidden message fun to 10 sec

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 var txtbox = $('.text-box');
 
 $(document).ready(function() {
-    // runs textBox after 7 secs
-    setTimeout(textBox, 7000);
+    // runs textBox after 10 secs
+    setTimeout(textBox, 10000);
     // closes after 40 secs
     setTimeout(closeBox, 40000);
 
