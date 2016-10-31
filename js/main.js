4dcdb3ce61e41b08902ceb9c1797019a5907933f
Dana Ng <ngdana1@gmail.com>
2016-10-31 10:27:38 -0700

    removed popup

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
-var txtbox = $('.text-box');
+// var txtbox = $('.text-box');
 
-$(document).ready(function() {
-    // runs textBox after 10 secs
-    setTimeout(textBox, 10000);
-    // closes after 40 secs
-    setTimeout(closeBox, 40000);
+// $(document).ready(function() {
+//     // runs textBox after 10 secs
+//     setTimeout(textBox, 10000);
+//     // closes after 40 secs
+//     setTimeout(closeBox, 40000);
 
-    // closes text-box 
-    $('.close-text').on('click', function() {
-        txtbox.slideToggle();
-    });
+//     // closes text-box 
+//     $('.close-text').on('click', function() {
+//         txtbox.slideToggle();
+//     });
 
-});
+// });
 
 
-function textBox() {
-    txtbox.slideToggle();
-}
+// function textBox() {
+//     txtbox.slideToggle();
+// }
 
-function closeBox() {
-    txtbox.css('display', 'none');
-}
+// function closeBox() {
+//     txtbox.css('display', 'none');
+// }
