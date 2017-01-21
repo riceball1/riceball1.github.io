47c45f9fe38fd371deaeaa99b7ce4d2322b9a10a
Dana Ng <ngdana1@gmail.com>
2017-01-20 17:51:38 -0800

    improved index content and moved coffee button

--- js/scripts-dist.js
+++ js/scripts-dist.js
@@ -5,10 +5,10 @@
 function describeMe() {
 	// var supportMsg = $('.support-msg');
 	var describe = $('.describe-me');
-	var adjectives = ['I\'m a minimalist', 'I\'m a female in tech', 'I love the outdoors', 'I believe in myself', 'I love to learn', 'I think about the future'];
+	var adjectives = ['a minimalist', 'a female in tech', 'an avid hiker', 'confident', 'a life-long learner', 'excited about the future', 'a web developer'];
 	var randomNum = Math.floor(Math.random() * 5);
 	describe.html(adjectives[randomNum]);
 }
 
-setInterval(describeMe, 5000);
+setInterval(describeMe, 8000);
 //# sourceMappingURL=scripts-dist.js.map
