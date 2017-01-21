229ad008ffd0c3c489be97497ca8d3b1461db3ab
Dana Ng <ngdana1@gmail.com>
2017-01-20 17:39:27 -0800

    added a buy me coffee button and added js to h1 in index

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// change saying for .support-msg
+
+function describeMe() {
+	// var supportMsg = $('.support-msg');
+	var describe = $('.describe-me');
+	var adjectives = ['I\'m a minimalist', 'I\'m a female in tech', 'I love the outdoors', 'I believe in myself', 'I love to learn', 'I think about the future'];
+		var randomNum = Math.floor(Math.random() * 5);
+		describe.html(adjectives[randomNum]);
+}
+
+setInterval(describeMe, 5000);
+
+
+
+
+
