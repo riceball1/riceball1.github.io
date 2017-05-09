5e45d0dd7aba6b566ba295cb6a974f480f19c922
Dana Ng <ngdana1@gmail.com>
2017-05-08 18:50:11 -0700

    removed focusing

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,9 @@
+
+/** 
+Attribution for smooth scrolling from:
+ https://css-tricks.com/snippets/jquery/smooth-scrolling/ 
+ **/
+
 // Select all links with hashes
 $('a[href*="#"]')
   // Remove links that don't actually link to anything
@@ -23,12 +29,12 @@ $('a[href*="#"]')
           // Callback after animation
           // Must change focus!
           var $target = $(target);
-          $target.focus();
+          // $target.focus();
           if ($target.is(":focus")) { // Checking if the target was focused
             return false;
           } else {
             $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
+            // $target.focus(); // Set focus again
           };
         });
       }
