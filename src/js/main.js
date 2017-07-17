6948ae633d31fdf146ec0ac4e39050a066a78b2d
Dana Ng <ngdana1@gmail.com>
2017-07-17 00:29:33 -0700

    changed description in array

--- src/js/main.js
+++ src/js/main.js
@@ -42,7 +42,7 @@ $('a[href*="#"]')
   });
 
 function startSubtitle() {
-  var list = ['minimialist', 'amateur polyglot', 'crazy foodie', ' full stack web developer'];
+  var list = ['minimialist', 'amateur polyglot', 'crazy foodie', ' web developer'];
   var count = 0;
   setInterval(function() {
       $('#subtitle').text(list[count % list.length]);
