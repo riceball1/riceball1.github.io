f4b05303779913110970c0e2d34c5c22d76b173a
Dana Ng <dn646y@wayfair.com>
2019-08-23 21:50:53 -0400

    Updates

--- README.md
+++
@@ -1,8 +0,0 @@
-# riceball1.github.io
-
-
-## Introduction
-This website hosted by github. I began developing this website on April 2016, and so far I have hosted several projects on github. I am continously expanding my knowledge in several areas of web development, including using the latest improvements on HTML, CSS, and JavaScript. Hopefully more updates to this page in the future.
-
-## Technologies:
-- Gulp: to manage tasks such as turning less files into css files [I have mind globally installed using ```npm install gulp-cli -g```]
---
+++ precache-manifest.605057e52a980e18746a75df8500fb45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79bf1500305dabd1ca6a3e58c5ce76a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "485896d4073a4b16ee2e",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b41c74c1c7444b5f4fef",
+    "url": "/static/js/2.48ea3bc3.chunk.js"
+  },
+  {
+    "revision": "485896d4073a4b16ee2e",
+    "url": "/static/js/main.31885102.chunk.js"
+  },
+  {
+    "revision": "e8eb0daa6dd62c9cc9aa",
+    "url": "/static/js/runtime~main.46b47ad8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
