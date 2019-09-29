91281fbbf4513348d335ff6dc40b6e8991ab2fe8
Dana Ng <dn646y@wayfair.com>
2019-09-29 15:50:27 -0400

    Added toggle button

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import {ThemeProvider} from 'styled-components';
 import {lightTheme, darkTheme} from '../styles/theme';
 import {GlobalStyles} from '../styles/global';
+import {CheckBoxWrapper, CheckBox, CheckBoxLabel} from '../styles/ToggleButton/ToggleButton';
 import './App.scss';
 
 // The main entry point of my website
@@ -19,8 +20,11 @@ function App() {
     <>
       <GlobalStyles />
       <div>
-
-        <button onClick={toggleTheme}>toggle theme</button>
+        <CheckBoxWrapper>
+          <CheckBoxLabel htmlFor="checkbox"/>
+          <CheckBox id="checkbox" type="checkbox" onChange={toggleTheme}/>
+          <p>{`${theme} mode`}</p>
+        </CheckBoxWrapper>
         <h1>Welcome to the {theme} side!</h1>
         <p>This is the website of <a href="https://www.linkedin.com/in/danafng/" target="_blank" rel="noopener noreferrer">Dana Ng.</a> </p> 
         <p>I'm a frontend engineer. </p>
