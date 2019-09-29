5acdddea2d5292d0e57ff63d68f1d2b09006b709
Dana Ng <dn646y@wayfair.com>
2019-09-29 15:51:36 -0400

    Updates

---
+++ precache-manifest.73277babde011c7c6e612d11323ae64e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "012c959d1ed92b307ba4b645ef6a2b49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b9a1577cbbc10f71c9d",
+    "url": "/static/css/main.7a244121.chunk.css"
+  },
+  {
+    "revision": "282b7360170a2f61979d",
+    "url": "/static/js/2.3abfdf67.chunk.js"
+  },
+  {
+    "revision": "3b9a1577cbbc10f71c9d",
+    "url": "/static/js/main.64debd33.chunk.js"
+  },
+  {
+    "revision": "e8eb0daa6dd62c9cc9aa",
+    "url": "/static/js/runtime~main.46b47ad8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8724c59f10bd3a49111196770eb33240.js"
+  "/precache-manifest.73277babde011c7c6e612d11323ae64e.js"
 );
 
 self.addEventListener('message', (event) => {
